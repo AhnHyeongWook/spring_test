3f513692c57708ab00a1201b9b5517b203113606
unknown <briaao@naver.com>
2018-07-02 12:01:17 +0900

    master 변경2

--- src/main/java/com/test/wookey/ApiController.java
+++ src/main/java/com/test/wookey/ApiController.java
@@ -23,11 +23,11 @@ public class ApiController {
 		String fileName = file.getOriginalFilename() + "_test";
 		int width = 250;
 		int height = 250;
-		String path1 = "C:\\Users\\wookey\\Desktop\\test\\" + fileName + "_ver12" + ".png";
+		String path1 = "C:\\Users\\wookey\\Desktop\\test\\" + fileName + "_ver1" + ".png";
 		String path2 = "C:\\Users\\wookey\\Desktop\\test\\" + fileName + "_ver2" + ".png";
 		File resizeFileVer1 = imageService.resizeVer1(image, fileName, width, height, path1);
 		File resizeFileVer2 = imageService.resizeVer2(image, fileName, width, height, path2);
 		
-		return "good";
+		return "good333";
 	}
 }
